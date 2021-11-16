d9408b54eebd2552c34a9c2bb35c2f0c13d9cfb1
Udit8158 <kunduudit8158@gmail.com>
2021-11-16 17:37:04 +0530

    add first time the config files

---
+++ config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+colorscript random
+
+##start starship
+
+# starship init fish | source
+
+#Aliases#
+alias ls='exa -al'
